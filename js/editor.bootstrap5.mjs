325663d6aa6dc9623d07efe54dc5e4e879b690bc
dtbuild <dtbuild@rigil>
2026-07-22 14:32:00 +0000

    a3360a316593f7ae8b4a09152b077bfee4347563 Release 3.0.0 ready
    Sync to source repo @a3360a316593f7ae8b4a09152b077bfee4347563

--- js/editor.bootstrap5.mjs
+++ js/editor.bootstrap5.mjs
@@ -1,4 +1,4 @@
-/*! Editor Bootstrap 5 styling 3.0.0-dev for DataTables
+/*! Editor Bootstrap 5 styling 3.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 
