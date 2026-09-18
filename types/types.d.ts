3236567ab45825423609785f400c921455b02b77
Allan Jardine <allan@datatables.net>
2026-09-18 15:38:37 +0000

    Dist: Fix types for the host Editor

--- types/types.d.ts
+++
@@ -1,7 +0,0 @@
-
-// Dist-DataTables-Editor-Bootstrap5 integration with Bootstrap5 exports the main Editor class having
-// set default values to complete the ingeration.
-import Editor from "datatables.net-editor";
-
-export default Editor;
-
